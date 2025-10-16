323a175567c647a48ec78cc409b7d5aae1fa4ab6
AzerQ <46249148+AzerQ@users.noreply.github.com>
2025-10-16 20:40:58 +0300

    Убрано ограничение на использование Swagger по средам
    Ранее Swagger и SwaggerUI включались только в среде разработки
    через проверку `if (app.Environment.IsDevelopment())`.
    Теперь эта проверка удалена, и Swagger с SwaggerUI
    включены для всех сред выполнения приложения.

--- Program.cs
+++ Program.cs
@@ -59,11 +59,10 @@ var app = builder.Build();
 var logger = app.Logger;
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
